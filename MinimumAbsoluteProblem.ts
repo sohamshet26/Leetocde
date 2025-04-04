ab5bad8514936455dbbba61a86ef3223eaa70c64
sohamshet26 <soham.shet2002@gmail.com>
2025-04-04 16:33:43 +0530

    projet setup and minimumAbsDiff solved

---
+++ MinimumAbsoluteProblem.ts
@@ -0,0 +1,27 @@
+//issue link: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+function minimumAbsDifference(arr: number[]): number[][] {
+    let working=[...arr]
+    working.sort((a,b)=>a-b)
+
+    //finding minimum absolute difference
+    let minAbsDiff = Infinity
+    const ans:number[][]=[]
+
+    for(let i=0;i<=working.length-1;i++){
+      if((working[i+1]-working[i])<minAbsDiff){
+        minAbsDiff=working[i+1]-working[i]
+      }
+    }
+    console.log(minAbsDiff)
+    for(let i=0;i<=working.length-1;i++){
+        if((working[i+1]-working[i])===minAbsDiff){
+            ans.push([working[i],working[i+1]])
+        }
+    }
+    console.log(ans)
+
+    return ans
+    
+};
+const ans:number[][]=minimumAbsDifference([4,2,1,3])
